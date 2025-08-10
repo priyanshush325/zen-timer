3eadce0d92395edbf5fa2fc60f2c10cb1b499191
priyanshush325 <priyanshush325@gmail.com>
2025-08-09 17:42:34 -0700

    fix: fix build issues caused by cubing.js package import

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        inlineDynamicImports: true
+      }
+    }
+  },
+  worker: {
+    format: 'es'
+  },
+  optimizeDeps: {
+    include: ['cubing/scramble']
+  }
 });
